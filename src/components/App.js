a64fa4d335b853c93ed7c2e58f3ad0876fdc1601
daveg7722 <davidgarzaonline@gmail.com>
2021-01-06 02:42:09 -0600

    added to webpack config

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1>A React App!</h1>
+            <h1>A React App</h1>
             <h2>Using Babel and Webpack!!</h2>
         </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports = {
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const config = {
+    mode: "development", 
     entry: './src/index.js',
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
@@ -9,9 +15,13 @@ module.exports = {
         open: true,
         hot: true
     },
-    //devtool: 'inline-source-map',
+    devtool: 'eval-source-map',
     module: {
         rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader','css-loader', 'sass-loader']
+            },
             {
             test: /\.js$/,
             include: [
@@ -21,15 +31,23 @@ module.exports = {
             use: {
                     loader: 'babel-loader',
                     options: {
-                        "presets": ["@babel/preset-env", "@babel/preset-react"]
+                        "presets": [["@babel/preset-env", {"useBuiltIns": "usage", "corejs" : 3, "targets": "defaults"}], "@babel/preset-react"]
                     }
                 }	
             }
         ]
     },
     output: {
-		filename: "main.js",
+		filename: "main.[hash].js",
         path: path.resolve(__dirname, "dist"), //__dirname is the root directory, where your project lives
 	},	
     plugins: [new HtmlWebpackPlugin({template: './src/index.html'})]
 }
+
+if (TARGET == "build") {
+    config.mode = "production";
+    config.plugins.push(new MiniCssExtractPlugin({filename: 'main.[hash].css'}), new CleanWebpackPlugin());
+    config.module.rules[0].use[0] = MiniCssExtractPlugin.loader;
+}
+
+module.exports = config;
